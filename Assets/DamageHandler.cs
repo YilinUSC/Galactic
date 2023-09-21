fd1f2b58e97475f1363440a10315e2889c6a3da4
Yilin4469 <yliu4860@usc.edu>
2023-09-20 18:15:46 -0700

    add game over script

--- Assets/DamageHandler.cs
+++ Assets/DamageHandler.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;  // If you're using TextMeshPro for the UI
 
 public class DamageHandler : MonoBehaviour
 {
     public int health = 1;
+    public GameObject gameOverUI;  // Drag your game over UI panel here in the inspector
     private int correctLayer;
+    
 
     void Start()
     {
         correctLayer = gameObject.layer;
+        
+        // Ensure the Game Over UI is hidden at start
+        if (gameOverUI)
+        {
+            gameOverUI.SetActive(false);
+        }
     }
-
+    
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Bullet")
@@ -23,12 +32,12 @@ public class DamageHandler : MonoBehaviour
 
                 if (enemyNumber && equationManager && equationManager.CheckAnswer(enemyNumber.number))
                 {
-                    // Correct enemy was hit, destroy the enemy.
+                    // Correct enemy was hit, destroy the enemy
                     Destroy(gameObject);
                 }
                 else
                 {
-                    // Wrong enemy was hit, damage the player.
+                    // Wrong enemy was hit, game over for the player
                     GameObject player = GameObject.FindGameObjectWithTag("Player");
                     if (player)
                     {
@@ -40,7 +49,7 @@ public class DamageHandler : MonoBehaviour
                     }
                 }
 
-                // Destroy the bullet after it hits something.
+                // Destroy the bullet after it hits something
                 Destroy(col.gameObject);
             }
         }
@@ -58,6 +67,19 @@ public class DamageHandler : MonoBehaviour
 
     void Die()
     {
+        if (gameObject.tag == "Player")
+        {
+            ShowGameOver();
+        }
         Destroy(gameObject);
+    }
+
+    void ShowGameOver()
+    {
+        if (gameOverUI)
+        {
+            gameOverUI.SetActive(true);  // Show the Game Over UI
+        }
+        Time.timeScale = 0;  // Pause the game
     }
 }
--- Assets/PlayerSpawner.cs
+++ Assets/PlayerSpawner.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class PlayerSpawner : MonoBehaviour
 {
     public GameObject playerPrefab;
+    public int numLives = 3;
     private GameObject playerInstance;
     private float respawnTimer;
 
     void Start()
     {
-        SpawnPlayer();
+        //SpawnPlayer();
     }
 
     void SpawnPlayer()
     {
+        numLives--;
         respawnTimer = 1;
         playerInstance = (GameObject)Instantiate(playerPrefab, transform.position, Quaternion.identity);
     }
@@ -22,13 +24,25 @@ public class PlayerSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerInstance == null)
+        if (playerInstance == null && numLives > 0)
         {
             respawnTimer -= Time.deltaTime;
             if (respawnTimer <= 0)
             {
                 SpawnPlayer();
             }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (numLives > 0 || playerInstance != null)
+        {
+            GUI.Label(new Rect(0, 0, 100, 50), "Lives Left: " + numLives);
+        }
+        else
+        {
+            GUI.Label(new Rect(0, 0, 10, 10), "Game Over!");
         }
     }
 }
